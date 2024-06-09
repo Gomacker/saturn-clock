f16e9ebd083d09d5969eb7ba5685b578773b799f
Gomacker <gomackerrst@gmail.com>
2024-06-09 17:42:13 +0800

    Commit

--- electron/main.ts
+++ electron/main.ts
@@ -1,9 +1,8 @@
-import { app, BrowserWindow } from 'electron'
+import {app, BrowserWindow, ipcMain, clipboard} from 'electron'
 import path from 'node:path'
-import {ipcMain} from "electron"
-import {ClipboardFormat} from "./libs/clipboard-native";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-import {getClipboardData, getClipboardFormats} from "./libs/clipboard-native";
+import type {ClipboardFormat} from "./libs/clipboard-native"
+// import {} from "./libs/clipboard-native";
+const {getClipboardData, getClipboardFormats, ansi2utf8, onClipboardUpdate} = require("./libs/clipboard-native");
 
 // The built directory structure
 //
@@ -23,81 +22,116 @@ let win: BrowserWindow | null
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 
 function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      devTools: true
-    },
-    // titleBarStyle: "hidden",
-    // hasShadow: true,
-    // frame: false,
-    // titleBarOverlay: {
-    //   color: '#2f3241',
-    //   symbolColor: '#74b1be',
-    //   height: 0,
-    // },
-    // backgroundMaterial: "acrylic",
-    // transparent: true
-  })
-  // const win2 = new BrowserWindow({})
-  win.webContents.openDevTools({mode: 'detach'})
-  // const hwnd = win.getNativeWindowHandle()
-  // const windowsTitleBar = require('electron-windows-titlebar')
-  // console.log(hwnd)
-  // console.log(windowsTitleBar);
+    win = new BrowserWindow({
+        icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            devTools: true
+        },
+        // titleBarStyle: "hidden",
+        // hasShadow: false,
+        hasShadow: true,
+        frame: false,
+        width: 800,
+        height: 600,
+        resizable: false,
+        // titleBarOverlay: {
+        //   color: '#2f3241',
+        //   symbolColor: '#74b1be',
+        //   height: 0,
+        // },
 
-  // win.setMinimumSize(200, 200)
-  // win.setMaximumSize(1600, 1200)
+        // backgroundMaterial: "acrylic",
+        // vibrancy: "selection",
+        // transparent: true
+    })
+    // const win2 = new BrowserWindow({})
+    win.webContents.openDevTools({mode: 'detach'})
+    // const hwnd = win.getNativeWindowHandle()
+    // const windowsTitleBar = require('electron-windows-titlebar')
+    // console.log(hwnd)
+    // console.log(windowsTitleBar);
 
-  ipcMain.on('getClipboard', (event) => {
-    // const text = clipboard.readText();
-    // const image = clipboard.readImage();
-    // const bookmark = clipboard.readBookmark();
-    // const available = clipboard.availableFormats()
-    const available = getClipboardFormats();
-    let _t: any = {
-      formats: available,
-      buffers: Object.fromEntries(available.map((value: ClipboardFormat) => [value.id, getClipboardData(value.id)])),
+    // win.setMinimumSize(200, 200)
+    // win.setMaximumSize(1600, 1200)
+
+    // clipboard.
+
+    ipcMain.on('getClipboard', (event) => {
+        // console.log(path.resolve('./'))
+        const available = getClipboardFormats();
+        let _t: any = {
+            formats: available,
+            buffers: Object.fromEntries(available.map((value: ClipboardFormat) => [value.id, getClipboardData(value.id)])),
+        }
+        // _t = {
+        //   ..._t,
+        //   // utf8Content: Object.fromEntries(
+        //   //     Object.entries(_t.buffers).map(([key, value]: [string, Buffer]) => [key, value.toString('utf-8')])
+        //   // )
+        // }
+        event.sender.send('clipboardContent', _t)
+        // event.sender.send('clipboardContent', {'cp': 'unavailable'})
+    })
+
+    ipcMain.on('ansi2utf8', (event, b: Uint8Array) => {
+        event.sender.send('ansi2utf8Callback', ansi2utf8(b))
+    })
+
+    ipcMain.on('readUtf16', (event, u8a: Uint8Array) => {
+        const b = Buffer.from(u8a)
+        let s = b.toString('utf-16le')
+        if (s.at(-1) === '\0') {
+            s = s.slice(0, -1)
+        }
+        event.sender.send('readUtf16Callback', s)
+    })
+
+    onClipboardUpdate(() => {
+        // console.log('teistno')
+        // just send a clipboard update event
+        win?.webContents.send('clipboardContent', (() => {
+            const available = getClipboardFormats();
+            console.log(available)
+            let _t: any = {
+                formats: available,
+                buffers: Object.fromEntries(available.map((value: ClipboardFormat) => [value.id, getClipboardData(value.id)])),
+            }
+            return _t
+        })())
+    })
+
+    // Test active push message to Renderer-process.
+    win.webContents.on('did-finish-load', () => {
+        win?.webContents.send('main-process-message', (new Date).toLocaleString())
+    })
+
+    if (VITE_DEV_SERVER_URL) {
+        win.loadURL(VITE_DEV_SERVER_URL).then(() => {
+        })
+    } else {
+        // win.loadFile('dist/index.html')
+        win.loadFile(path.join(process.env.DIST, 'index.html')).then(() => {
+        })
     }
-    _t = {
-      ..._t,
-      utf8Content: Object.fromEntries(
-          Object.entries(_t.buffers).map(([key, value]: [string, Buffer]) => [key, value.toString('utf-8')])
-      )
-    }
-    event.sender.send('clipboardContent', _t)
-  })
-
-  // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
-
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL).then(() => {})
-  } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(process.env.DIST, 'index.html')).then(() => {})
-  }
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-    win = null
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+        win = null
+    }
 })
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
 
 app.whenReady().then(createWindow)
--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,281 @@
-import {ReactNode, useEffect, useState} from 'react'
+import {Component, ReactNode, useEffect, useState} from 'react'
 import './App.css'
 import './test.css'
-import {css} from "@emotion/css"
-import {Button, Card, Divider} from "@mui/material";
+import {css, cx} from "@emotion/css"
+import {
+    AppBar,
+    Box,
+    Button,
+    Card,
+    CardContent,
+    Icon,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    Toolbar,
+    Typography
+} from "@mui/material";
 import {ClipboardFormat} from "../electron/libs/clipboard-native";
+import * as Icons from "@mui/icons-material";
 
 
 let test = false;
 
+function InfoCard(): ReactNode {
+    return (
+        <Card className={css`
+            position: absolute;
+            bottom: 6px;
+        `}>
+            <CardContent className={css`
+                display: flex;
+                align-items: center;
+            `}>
+                <Icon color="info" style={{overflow: "initial", marginRight: "8px"}}>
+                    <Icons.Info></Icons.Info>
+                </Icon>
+                <Box>
+                    <div className={css`
+                        font-weight: bold;
+                    `}>
+                        测试Info
+                    </div>
+                    <Typography variant="body2">
+                        this is a test info content
+                    </Typography>
+                </Box>
+            </CardContent>
+        </Card>
+    )
+}
+
+class InfoRegin2 extends Component<{ className?: string }> {
+    info(title: string, content: string, duration: number = 5): void {
+
+    }
+
+    render(): ReactNode {
+        return <></>
+    }
+}
+
+function InfoRegin({className}: { className?: string }): ReactNode {
+    return (
+        <Box className={cx(
+            css``,
+            className
+        )}>
+            <InfoCard></InfoCard>
+            <InfoCard></InfoCard>
+            <InfoCard></InfoCard>
+            <InfoCard></InfoCard>
+            <InfoCard></InfoCard>
+            <InfoCard></InfoCard>
+            <InfoCard></InfoCard>
+        </Box>
+    )
+}
+
+declare interface Window {
+    ipcRenderer: import('electron').IpcRenderer
+}
+
 function App(): ReactNode {
-    const [content, setContent] = useState<{formats: ClipboardFormat[], buffers: {[key: string]: Buffer}}>({formats: [], buffers: {}})
-    const [utf8Content, setUtf8Content] = useState<{ [key: string]: string }>({})
+    const [content, setContent] = useState<{
+        formats: ClipboardFormat[],
+        buffers: { [key: string]: Buffer }
+    }>({formats: [], buffers: {}})
+
+    const [plainText, setPlainText] = useState<string>('');
 
     useEffect(() => {
         if (!test) {
             test = true
-            setInterval(() => {
-                window.ipcRenderer.send('getClipboard')
-            }, 3000);
+            window.ipcRenderer.removeAllListeners('clipboardContent')
             window.ipcRenderer.on('clipboardContent', (event, _content) => {
                 console.log(_content)
-                setContent(_content)
-                // Object.entries(_content.buffers).forEach(([key, value]: [string, Uint8Array]) => {
-                //     (new Blob([value])).text().then(value1 => {
-                //         setUtf8Content({...utf8Content, [key]: value1})
-                //     })
-                // })
+                // setContent(_content)
+                if (13 in _content.buffers) {
+                    window.ipcRenderer.send('readUtf16', _content.buffers[13])
+                }
             })
+
+            window.ipcRenderer.on('readUtf16Callback', (event, result: string) => {
+                // console.log(result)
+                setPlainText(result.trim())
+            })
+            window.ipcRenderer.on('ansi2utf8Callback', (event, result: string) => {
+                // console.log(result)
+                setPlainText(result.trim())
+            })
+            window.ipcRenderer.send('getClipboard')
         }
     }, []);
-    // return (
-    //     <div className={css`
-    //         //background: red;
-    //         height: 100vh;
-    //         width: 100vw;
-    //         //background: lightcoral;
-    //         border-radius: 4px;
-    //         overflow: hidden;
-    //         display: flex;
-    //         justify-content: center;
-    //         align-items: center;
-    //     `}>
-    //         <div className={css`width: 180px`}>
-    //             {content.formats.map((format, key) => (
-    //                 <Card key={key}>
-    //                     <div className={css`
-    //                     background: lightcoral;
-    //                     color: white;
-    //                     padding: 2px 4px;
-    //                 `}>
-    //                         {format.name || format.id}
-    //                     </div>
-    //                     <Divider/>
-    //                     <div>
-    //                         {content.utf8Content[format.id]}
-    //                     </div>
-    //                 </Card>
-    //             ))}
-    //         </div>
-    //     </div>
-    // )
-    return "dfff"
+    return <Box className={css`
+        //display: flex;
+        //flex-direction: column;
+        //justify-content: center;
+        //align-items: center;
+        //border-radius: 10px;
+        height: 100vh;
+        width: 100vw;
+        overflow: hidden;
+    `}>
+        <AppBar>
+            <Toolbar className={css`
+                -webkit-app-region: drag;
+                justify-content: space-between;
+            `} variant="dense">
+                <Typography>Saturn Clock</Typography>
+                <Button className={css`-webkit-app-region: no-drag;`} color="inherit">close</Button>
+            </Toolbar>
+        </AppBar>
+
+        <Box className={css`
+            //border-radius: inherit;
+            display: flex;
+            flex-direction: column;
+            //justify-content: center;
+            //align-items: center;
+            height: 100%;
+            width: 100%;
+            background: #407183;
+            //border: #ccc 1px solid;
+            box-sizing: border-box;
+        `}>
+
+            <Toolbar variant="dense"/>
+
+            <Box className={css`
+                display: flex;
+                flex-grow: 1;
+            `}>
+                <Box className={css`
+                    //position: absolute;
+                    padding: 8px;
+                    //height: calc(100% - 16px);
+                `}>
+                    <Card className={css`
+                        height: 100%
+                    `}>
+                        <List className={css`
+                            width: 25vw;
+                        `}>
+                            {[
+                                'Home',
+                                'Math',
+                                'Development',
+                                'Image',
+                                'Audio',
+                                'File',
+                                'Custom',
+                            ].map((item) => (
+                                <ListItem key={item} style={{padding: '4px'}}>
+                                    <ListItemButton>
+                                        {item}
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Card>
+                </Box>
+                <Box className={css`
+                    flex-grow: 1;
+                    //display: grid;
+                    //padding: 8px;
+                    //grid-template-rows: repeat(auto-fit, 120px);
+                    //grid-gap: 8px;
+                `}>
+                    {[
+                        {name: 'origin', func: (value: any) => value},
+                        {name: 'uppercase', func: (value: string) => value.toUpperCase()},
+                        {name: 'lowercase', func: (value: string) => value.toLowerCase()},
+                        {name: 'reversed', func: (value: string) => value.split('').reverse().join('')},
+                        {
+                            name: 'digital', func: (value: string) => {
+                                const l = value.split(/[^0-9]/g).filter(value => !!value)
+                                return parseInt(l[0]).toString()
+                            }
+                        },
+                    ]
+                        .map((value, index) => (
+                            <Card key={index} className={css`
+                                display: flex;
+                                margin: 8px;
+                                overflow: hidden;
+                                height: fit-content;
+                            `}>
+                                <Box className={css`
+                                    width: 120px;
+                                    min-width: 120px;
+                                    background: #53f2f2;
+                                    display: flex;
+                                    justify-content: center;
+                                    align-items: center;
+                                    //color: white;
+                                    //border-radius: ;
+                                    user-select: none;
+                                `}>
+                                    {value.name}
+                                </Box>
+                                <Box className={css`
+                                    padding: 16px;
+                                    flex-grow: 1;
+                                    font-weight: bold;
+                                    user-select: all;
+                                    white-space: pre-wrap;
+                                `}>{value.func(plainText)}</Box>
+                                <Box className={css`
+                                    width: 120px;
+                                    background: red;
+                                    padding: 8px;
+                                    box-sizing: border-box;
+                                    //display: grid;
+                                    display: none;
+                                    grid-template-columns: repeat(2, 50%);
+                                    grid-template-rows: repeat(2, 50%);
+                                    //grid-gap: 8px;
+                                `}>
+                                    <Box className={css`
+                                        width: 100%;
+                                        height: 100%;
+                                        background: white;
+                                        padding: 8px;
+                                        box-sizing: border-box;
+                                    `}>
+                                        <IconButton>
+                                            {/*<Icon>*/}
+                                            {/*    */}
+                                            {/*</Icon>*/}
+                                        </IconButton>
+                                    </Box>
+                                    <Box className={css`
+                                        width: 100%;
+                                        height: 100%;
+                                        background: white;
+                                    `}>
+
+                                    </Box>
+                                </Box>
+                            </Card>
+                        ))}
+                    {/*<Card className={css`*/}
+                    {/*    padding: 16px;*/}
+                    {/*`}>*/}
+                    {/*    reverse: {plainText.split('').reverse().join('')}*/}
+                    {/*</Card>*/}
+                </Box>
+            </Box>
+        </Box>
+        <InfoRegin className={css`
+            position: fixed;
+            bottom: 8px;
+            right: 8px;
+            width: 240px;
+        `}></InfoRegin>
+        <InfoRegin2></InfoRegin2>
+    </Box>
 }
 
 export default App
